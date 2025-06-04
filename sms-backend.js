0f718301c6734a8f4bb1d1ded7e74bcff80deec1
Marciojenys <marciojenys@me.com>
2025-06-04 11:08:12 -0300

    Update sms-backend.js

--- sms-backend.js
+++ sms-backend.js
@@ -30,5 +30,5 @@ app.post('/enviar-sms', async (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log('Backend de SMS rodando em http://localhost:3000');
+  console.log('Backend de SMS rodando em https://github.com/Marciojenys/meionorteexpress.git');
 });
